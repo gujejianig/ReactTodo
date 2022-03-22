8914c07dc56573280de1bbc4e3347f76e27c318c
gujejianig <gujejiani.websmart@gmail.com>
2022-03-22 13:36:46 +0400

    todo app functional completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const submitHandler = (e) => {
+		e.preventDefault();
+		if (!inputValue) {
+			alert('input have no value');
+		} else {
+			const newItem = {task: inputValue, id: Math.random()};
+			console.log(newItem);
+			// adding to Todos - main data
+			setTodos([...todos, newItem]);
+		}
+		setInputValue('');
+	};
+
+		const handleKeyDown = (event) => {
+			if (event.key === 'Enter') {
+				submitHandler(event)
+			}
+		}
+
+	return (
+		<>
+			<div className="Container">
+				<div className="d-flex w-100 justify-content-sm-center mt-3">
+					<input onKeyUp={handleKeyDown} value={inputValue} style={{width: '100%', borderRadius: '5px'}}
+					       onChange={(e) => setInputValue(e.target.value)}/>
+					<Button size="sm" onClick={submitHandler} variant="primary">Add</Button>
+				</div>
+				{todos?.map((item) => {
+					return (
+						<div className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center">
+							<span  style={{fontSize: "24px"}}>{item.task}</span>
+							<Button className="m-lg-2" size="sm"  variant="danger">Remove</Button>
+							<Button className="" size="sm"  variant="info">edit</Button>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
 
 export default App;
