7101ff678251c752acb01bd03812f6a8f33a30d7
gujejianig <gujejiani.websmart@gmail.com>
2022-03-23 13:32:36 +0400

    Revert "checking 'HELLO'"
    This reverts commit 6639b474d5d4228e39cb03fa92e38d464d62e439.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,23 +1,25 @@
 import React from "react";
 import {Button} from 'react-bootstrap';
 
-const Pagination = ({todos, todosPerPage, onPaginatedList}) => {
+const Pagination = ({todos, todosPerPage, onPaginatedList, paginationButtons}) => {
 
-	let paginationButtons = [];
+	paginationButtons = [];
 	for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
 		paginationButtons.push(i);
 	}
 
-	return (
-		<>
-			<div className="d-flex">
-				{paginationButtons?.map((btn, index) => {
-					return <div key={index}><Button onClick={() => onPaginatedList(index + 1)} size="sm" className="m-1"
-					                                variant="outline-dark">{btn}</Button></div>;
-				})}
-			</div>
-		</>
-	);
+// Hello
+
+	return (<>
+		<div className="d-flex">
+			{paginationButtons?.map((btn, index) => {
+				return <div key={index}>
+					<Button onClick={() => onPaginatedList(index + 1)} size="sm" className="m-1"
+					        variant="outline-dark">{btn}</Button>
+				</div>;
+			})}
+		</div>
+	</>);
 };
 // HELLLO  COMMIT 1
 export default Pagination;
