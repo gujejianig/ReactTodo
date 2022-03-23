6639b474d5d4228e39cb03fa92e38d464d62e439
gujejianig <gujejiani.websmart@gmail.com>
2022-03-23 13:31:35 +0400

    checking 'HELLO'

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,23 @@
 import React from "react";
 import {Button} from 'react-bootstrap';
 
-const Pagination = ({todos, todosPerPage, onPaginatedList, paginationButtons}) => {
+const Pagination = ({todos, todosPerPage, onPaginatedList}) => {
 
-	paginationButtons = [];
+	let paginationButtons = [];
 	for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
 		paginationButtons.push(i);
 	}
 
-// Hello
-
-	return (<>
-		<div className="d-flex">
-			{paginationButtons?.map((btn, index) => {
-				return <div key={index}>
-					<Button onClick={() => onPaginatedList(index + 1)} size="sm" className="m-1"
-					        variant="outline-dark">{btn}</Button>
-				</div>;
-			})}
-		</div>
-	</>);
+	return (
+		<>
+			<div className="d-flex">
+				{paginationButtons?.map((btn, index) => {
+					return <div key={index}><Button onClick={() => onPaginatedList(index + 1)} size="sm" className="m-1"
+					                                variant="outline-dark">{btn}</Button></div>;
+				})}
+			</div>
+		</>
+	);
 };
 
 export default Pagination;
