7b038a17e8aa9c36d1eb763897c3ecfe942856d8
gujejianig <gujejiani.websmart@gmail.com>
2022-03-29 16:53:04 +0400

    Update README.md

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -6,34 +6,36 @@ const TodosList = ({todos, setTodos, todosPerPage, activePage}) => {
 
 	const [editing, setEditing] = useState(false);
 	const [editInput, setEditInput] = useState('');
+	const [editedItem, setEditedItem] = useState('')
+	const [name, setName] = useState('')
+	const [editId, setEditId] = useState(null)
 	const removeHandler = (id) => {
 		setTodos(todos.filter((todo) => {
 			return todo.id !== id;
 		}));
 	};
 	const editHandler = (id) => {
-		// const filteredItems = todos.filter((todo) => {index
-		// 	return todo.id !== index
-		// })
-		const selectedTodo = todos.find((item) => {
-			return item.id === id;
-		});
+		const selectedTodo = todos.find(item => item.id === id)
 
-		setEditing(!editing);
-		console.log(selectedTodo);
+		if(!editing) {
+			setEditing(true)
+			setEditId(id)
+			setName(selectedTodo.task)
+		} if(editing) {
+			selectedTodo = name
+		}
 	};
 	let end = activePage * todosPerPage;
 	let start = end - todosPerPage;
 	return (<>
-		{todos.slice(start, end)?.map((item, index) => {
-			return (<div key={index} className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center">
-				{editing ?
-					<input style={{fontSize: "24px"}} onChange={(e) => setEditInput(e.target.value)} value={item.task}/> :
-					<span style={{fontSize: "24px"}}>{item.task}</span>}
+		{todos.slice(start, end)?.map((item) => {
+			return (<div key={item.id} className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center">
+					<span style={{fontSize: "24px"}}>{item.task}</span>
 				<Button onClick={() => removeHandler(item.id)} className="m-lg-2" size="sm" variant="danger">Remove</Button>
-				<Button onClick={() => editHandler(item.id)} className="" size="sm" variant="info">edit</Button>
+				<Button onClick={() => editHandler(item.id)} className="" size="sm" variant="info">{editing ? "save": "edit"}</Button>
 			</div>);
 		})}
+		<input value={name} onChange={(e) => setName(e.target.value)}/>
 	</>);
 
 };
